b91cb47f04c85e543d333f5c0091746192e1912e
kurumakazuto <153343673+kurumakazuto@users.noreply.github.com>
2024-01-08 15:12:53 +0000

    [Finish]3-3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,6 +11,16 @@ class ListsController < ApplicationController
     redirect_to '/top'
   end
 
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
   private
   def list_params
     params.require(:list).permit(:title, :body)
